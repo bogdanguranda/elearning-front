7c643ac631ebbf44550746b1716d3f4bacb852d4
bogdanguranda <bogdanguranda@gmail.com>
2015-12-28 13:45:36 +0200

    Fixed naming and garbage files.

--- app/components/forum/scripts/forumTopics.js
+++
@@ -1,9 +0,0 @@
-$(".button").click(function () {
-    $("#sForm").toggleClass("open");
-});
-
-$(".controlTd").click(function () {
-    $(this).children(".settingsIcons").toggleClass("display");
-    $(this).children(".settingsIcon").toggleClass("openIcon");
-});
-
